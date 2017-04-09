b0664271550102236c7d45fc3ff77323f38356e7
Tom Kim <tom.kim@berkeley.edu>
2017-04-09 15:53:00 -0700

    finished lab6

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -24,7 +24,8 @@ class LoginViewController: UIViewController {
         static let viewColor = UIColor.red
         static let textColor = UIColor.red
         
-        static let titleTextSize: CGFloat = 36
+        static let textSize: CGFloat = 20
+        static let titleSize: CGFloat = 40
     
     }
 
@@ -32,10 +33,9 @@ class LoginViewController: UIViewController {
     var myTitle: UILabel = {
         let myTitle = UILabel()
         myTitle.frame = CGRect(x: 0
-, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height * 0.2)
-//        myTitle.backgroundColor = UIColor.orange
-        myTitle.textColor = UIColor.black
-        myTitle.font = UIFont(name: "Avenir", size: Constants.titleTextSize)
+, y: UIScreen.main.bounds.midY/4, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height * 0.2)
+        myTitle.textColor = UIColor.yellow
+        myTitle.font = UIFont(name: "Times New Roman", size: Constants.titleSize)
         myTitle.textAlignment = NSTextAlignment.center
         myTitle.text = "Login View Controller"
         return myTitle
@@ -48,59 +48,66 @@ class LoginViewController: UIViewController {
         myView.backgroundColor = UIColor.white
         myView.layer.cornerRadius = Constants.buttonCornerRadius
         myView.layer.masksToBounds = true
-
-        var myId: UITextField = {
-            let myId = UITextField()
-            myId.frame = CGRect(x: 0, y: (UIScreen.main.bounds.midY/2), width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.1)
-            myId.placeholder = "Enter your email"
-            myId.font = UIFont(name: "Avenir", size: Constants.titleTextSize)
-            myId.borderStyle = UITextBorderStyle.roundedRect
-            myId.autocorrectionType = UITextAutocorrectionType.no
-            myId.keyboardType = UIKeyboardType.default
-            myId.returnKeyType = UIReturnKeyType.done
-//            myId.backgroundColor = UIColor.gray
-            return myId
-        }()
-        
-        myView.addSubview(myId)
         
         return myView
     }()
     
-//    var myId: UITextField = {
-//        let myId = UITextField()
-//        myId.frame = CGRect(x: 0, y: UIScreen.main.bounds.midY/2, width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.1)
-//        myId.backgroundColor = UIColor.gray
-//        return myId
-//    }()
-
+    var myId: UITextField = {
+        let myId = UITextField()
+        myId.frame = CGRect(x: UIScreen.main.bounds.size.width * 0.05, y: (UIScreen.main.bounds.midY/2 - UIScreen.main.bounds.size.height * 0.24), width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.08)
+        myId.placeholder = "Enter your email"
+        myId.font = UIFont(name: "Times New Roman", size: Constants.textSize)
+        myId.borderStyle = UITextBorderStyle.roundedRect
+        myId.autocorrectionType = UITextAutocorrectionType.no
+        myId.keyboardType = UIKeyboardType.default
+        myId.returnKeyType = UIReturnKeyType.done
+        myId.textAlignment = NSTextAlignment.center
+        return myId
+    }()
     
-    var button: UIButton = {
+    var myPw: UITextField = {
+        let myPw = UITextField()
+        myPw.frame = CGRect(x: UIScreen.main.bounds.size.width * 0.05, y: (UIScreen.main.bounds.midY/2 - UIScreen.main.bounds.size.height * 0.14), width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.08)
+        myPw.placeholder = "Enter password"
+        myPw.font = UIFont(name: "Times New Roman", size: Constants.textSize)
+        myPw.borderStyle = UITextBorderStyle.roundedRect
+        myPw.autocorrectionType = UITextAutocorrectionType.no
+        myPw.keyboardType = UIKeyboardType.default
+        myPw.returnKeyType = UIReturnKeyType.done
+        myPw.textAlignment = NSTextAlignment.center
+        return myPw
+    }()
+    
+    var myButton: UIButton = {
         // first create a button to be returned by the closure
         let myButton = UIButton()
         
-        // configure the text of the button
+        let buttonWidth: CGFloat = UIScreen.main.bounds.size.width * 0.6
+        myButton.frame = CGRect(x: UIScreen.main.bounds.size.width * 0.05, y: (UIScreen.main.bounds.midY/2 - UIScreen.main.bounds.size.height * 0.04), width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.08)
+        
         myButton.setTitle("Login", for: .normal)
-        myButton.titleLabel?.font = UIFont(name: "Avenir", size: Constants.buttonTextSize)
+        myButton.titleLabel?.font = UIFont(name: "Times New Roman", size: Constants.buttonTextSize)
         myButton.titleLabel?.numberOfLines = 2
         myButton.titleLabel?.textAlignment = .center
-        myButton.setTitleColor(Constants.textColor, for: .normal)
+        myButton.setTitleColor(UIColor.yellow, for: .normal)
         
         // configure button itself
-        myButton.backgroundColor = Constants.buttonColor
+        myButton.backgroundColor = UIColor(hue: 0.5389, saturation: 1, brightness: 0.92, alpha: 1.0)
         // makes the corners of the cell rounded, instead of squared off
         myButton.layer.cornerRadius = Constants.buttonCornerRadius
         myButton.layer.masksToBounds = true
         
-        // Since we specified that the button is equal
-        // to the closure's return value (`button = { ... }())
-        // `button` will now equal `myButton`
+        myButton.addTarget(self, action: #selector(authenticate), for: .touchDown)
+        
         return myButton
     }()
     
-    
-    
-    
+    var myImageView: UIImageView = {
+        let myImageView = UIImageView(frame: CGRect(x: UIScreen.main.bounds.size.width * 0.1
+            , y: UIScreen.main.bounds.midY * 4/3, width: UIScreen.main.bounds.size.width * 0.8, height: UIScreen.main.bounds.size.height * 0.3))
+        myImageView.image = UIImage(named: "oskibear")
+        return myImageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -110,72 +117,21 @@ class LoginViewController: UIViewController {
         
         // TODO: layout your views using frames or AutoLayout
         myView.center = view.center
-//        myId.center = view.center
-//        myTitle.center = view.center
-        
-        // Calculate the width of the button based off the size of the view
-        let buttonWidth: CGFloat = UIScreen.main.bounds.size.width * 0.6
-        
-        // Create the frame for the button (necessary if not using constraints)
-        // I set the x and y values to 0 since I use button.center, so they do not matter
-        // I could use x and y to position my view, but this is extra math
-        button.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: Constants.buttonHeight)
-        
-        // This puts my button at the center x and y of the superview (view)
-        button.center = view.center
-        
-        // This is how you link buttons to methods programmatically (if we
-        // were using a storyboard we would control drag to our IBAction instead)
-        // `addTarget` takes in 3 parameters
-        //     1. target: this is where the action method is defined (in our case,
-        //          this is our MainViewController, or self)
-        //     2. action: the method we want to be called when the button (or other
-        //          UI element) is interacted with
-        //     3. for: (controlEvents) here we tell Xcode when we want the action
-        //          to be called. In this case, I put ".touchDown" since I want
-        //          "goToNextView" to be called every time the user touches down on the button
-        button.addTarget(self, action: #selector(goToNextView), for: .touchDown)
         
         // IMPORTANT: this adds our button to the view. If you do not call this,
         // the button will never show up on the screen
         self.view.addSubview(myTitle)
         self.view.addSubview(myView)
-        myView.addSubview(button)
-//        myView.addSubview(myId)
+        myView.addSubview(myId)
+        myView.addSubview(myPw)
+        myView.addSubview(myButton)
+        self.view.addSubview(myImageView)
     }
     
     // TODO: create an IBAction for your login button
-    @IBAction func goToNextView(sender: UIButton) {
-        let secondView = LoginViewController()
-        // presents the viewController specified, and calls whatever is passed
-        // in for `completion` once the view is presented. Since I don't have
-        // any use for a completion handler, I just put a print statement inside
-        // for fun (I could've left it nil as well)
-        self.present(secondView, animated: true, completion: {
-            print("This is called after the Second View Controller is presented!")
-        })
+    @IBAction func authenticate(sender: UIButton) {
+        authenticateUser(username: myId.text, password: myPw.text)
     }
-    
-//    // initializer for CGRect. This returns a rectangle with the given size and
-//    // position that can be used to set the frame of your UI elements.
-//    CGRect(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -
-//    
-//    // returns the size of the user's screen (you can use this to get the width and height of the screen)
-//    UIScreen.main.bounds.size
-//    
-//    // returns the center x coordinate of the user's screen (CGFloat)
-//    UIScreen.main.bounds.midX
-//    
-//    // returns the center y coordinate of the user's screen (CGFloat)
-//    UIScreen.main.bounds.midY
-//    
-//    // returns the CGPoint at the center of "view" (remember, ViewControllers have a
-//    // default "view" associated with them. This is the white (or blue in our app) background
-//    // that you see when the View Controller appears on the screen). Since this view is the same
-//    // size as the device's screen, you may use it instead of UIScreen.main to get the sizing information you need.
-//    view.center
-    
-    
     
     /// YOU DO NOT NEED TO MODIFY ANY OF THE CODE BELOW (but you will want to use `authenticateUser` at some point)
     
